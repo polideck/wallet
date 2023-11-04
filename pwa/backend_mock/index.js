89504156e8655c0c2bb1009a40e2f92ea8218098
eyan88 <yaneric.4d@gmail.com>
2023-11-04 11:16:04 -0700

    update middleware

--- pwa/backend_mock/index.js
+++ pwa/backend_mock/index.js
@@ -6,11 +6,24 @@ mongoose.connect('mongodb://127.0.0.1:27017/pwa');
 
 import router from './controllers/index.js';
 
+app.use((req, res, next) => {
+	const allowedOrigins = ["http://localhost:3000", "http://localhost:5000", "https://localhost:3000", "https://exilirate.com"];
+	const origin = req.headers.origin;
+	if (allowedOrigins.includes(origin)) {
+		res.setHeader('Access-Control-Allow-Origin', origin);
+	}
+
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	res.header('Access-Control-Allow-Credentials', true);
+	return next();
+});
+
+app.use('/api', router);
+
 app.get("/", (req, res) => {
     res.send("hello");
 })
-
-app.use('/api', router);
 
 app.listen(5050, () => {
     console.log("mock backend running on port 5050");
