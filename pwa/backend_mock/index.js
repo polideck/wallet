78eb280dfaf2e4a22e993ac3de235d9f71fa7cf3
eyan88 <yaneric.4d@gmail.com>
2023-11-14 14:39:28 -0700

    add mongodb remote connection, getNonce functions

--- pwa/backend_mock/index.js
+++ pwa/backend_mock/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/pwa');
+//mongoose.connect('mongodb://127.0.0.1:27017/pwa');
 
 import router from './controllers/index.js';
 
